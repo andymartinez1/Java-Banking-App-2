7b514da68c11c9246802e0f471f31ad802a14d32
Andrew Martinez <andy72391@gmail.com>
2024-09-10 20:48:34 -0400

    Documented the CRUD REST APIs using SpringDoc OpenAPI and Swagger UI. Included summary and description of the REST APIs and relevant status codes.

--- Accounts/src/main/java/com/andymartinez1/accounts/AccountsApplication.java
+++ Accounts/src/main/java/com/andymartinez1/accounts/AccountsApplication.java
@@ -1,11 +1,20 @@
 package com.andymartinez1.accounts;
 
+import io.swagger.v3.oas.annotations.OpenAPIDefinition;
+import io.swagger.v3.oas.annotations.info.Info;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
 @SpringBootApplication
 @EnableJpaAuditing(auditorAwareRef = "auditAwareImpl")
+@OpenAPIDefinition(
+        info = @Info(
+                title = "Banking Account Microservices REST API Documentation",
+                description = "Banking Account Microservices REST API Documentation",
+                version = "v1.0.0"
+        )
+)
 public class AccountsApplication {
 
     public static void main(String[] args) {
--- Accounts/src/main/java/com/andymartinez1/accounts/controller/AccountsController.java
+++ Accounts/src/main/java/com/andymartinez1/accounts/controller/AccountsController.java
@@ -4,6 +4,10 @@ import com.andymartinez1.accounts.constants.AccountConstants;
 import com.andymartinez1.accounts.dto.CustomerDTO;
 import com.andymartinez1.accounts.dto.ResponseDTO;
 import com.andymartinez1.accounts.service.IAccountService;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.responses.ApiResponses;
+import io.swagger.v3.oas.annotations.tags.Tag;
 import jakarta.validation.Valid;
 import jakarta.validation.constraints.Pattern;
 import lombok.AllArgsConstructor;
@@ -13,6 +17,10 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.*;
 
+@Tag(
+        name = "CRUD REST APIs for Accounts",
+        description = "Create, Read, Update and Delete Account Details"
+)
 @RestController
 @RequestMapping(path = "/api", produces = {MediaType.APPLICATION_JSON_VALUE})
 @Validated
@@ -21,6 +29,14 @@ public class AccountsController {
 
     private IAccountService iAccountService;
 
+    @Operation(
+            summary = "Create Account REST API",
+            description = "Create a new customer and account inside the banking app"
+    )
+    @ApiResponse(
+            responseCode = "201",
+            description = "HTTP Status CREATED"
+    )
     @PostMapping("/create")
     public ResponseEntity<ResponseDTO> createAccount(@Valid @RequestBody CustomerDTO customerDTO) {
         iAccountService.createAccount(customerDTO);
@@ -29,6 +45,14 @@ public class AccountsController {
                 .body(new ResponseDTO(AccountConstants.STATUS_201, AccountConstants.MESSAGE_201));
     }
 
+    @Operation(
+            summary = "Read Account Details REST API",
+            description = "Read customer and account details inside the banking app"
+    )
+    @ApiResponse(
+            responseCode = "200",
+            description = "HTTP Status OK"
+    )
     @GetMapping("/fetch")
     public ResponseEntity<CustomerDTO> fetchAccountDetails(@RequestParam
                                                            @Pattern(regexp = "(^$|[0-9]{10})", message = "Number must be 10 digits")
@@ -37,6 +61,20 @@ public class AccountsController {
         return ResponseEntity.status(HttpStatus.OK).body(customerDTO);
     }
 
+    @Operation(
+            summary = "Update Account REST API",
+            description = "Update customer and account inside the banking app"
+    )
+    @ApiResponses({
+            @ApiResponse(
+                    responseCode = "200",
+                    description = "HTTP Status OK"),
+            @ApiResponse(
+                    responseCode = "417",
+                    description = "HTTP Status Update Operation Failed"
+            )
+    }
+    )
     @PutMapping("/update")
     public ResponseEntity<ResponseDTO> updateAccountDetails(@Valid @RequestBody CustomerDTO customerDTO) {
         boolean isUpdated = iAccountService.updateAccount(customerDTO);
@@ -49,6 +87,20 @@ public class AccountsController {
         }
     }
 
+    @Operation(
+            summary = "Delete Account REST API",
+            description = "Delete customer and account inside the banking app"
+    )
+    @ApiResponses({
+            @ApiResponse(
+                    responseCode = "200",
+                    description = "HTTP Status OK"),
+            @ApiResponse(
+                    responseCode = "417",
+                    description = "HTTP Status Delete Operation Failed"
+            )
+    }
+    )
     @DeleteMapping("/delete")
     public ResponseEntity<ResponseDTO> deleteAccountDetails(@RequestParam
                                                             @Pattern(regexp = "(^$|[0-9]{10})", message = "Number must be 10 digits")
