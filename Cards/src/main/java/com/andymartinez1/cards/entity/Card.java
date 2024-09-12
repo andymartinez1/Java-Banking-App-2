606f3c91e541ccb7ecf7f01f9b334c37a1a39af8
Andrew Martinez <andy72391@gmail.com>
2024-09-12 06:08:13 -0400

    Added similar entity, DTO, mapper, controller, etc. packages as the accounts MS to loans and cards.

---
+++ Cards/src/main/java/com/andymartinez1/cards/entity/Card.java
@@ -0,0 +1,37 @@
+package com.andymartinez1.cards.entity;
+
+import jakarta.persistence.*;
+import lombok.*;
+
+@Entity
+@Getter
+@Setter
+@ToString
+@AllArgsConstructor
+@NoArgsConstructor
+public class Card {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "card_id")
+    private Long cardId;
+
+    @Column(name = "mobile_number")
+    private String mobileNumber;
+
+    @Column(name = "card_number")
+    private int cardNumber;
+
+    @Column(name = "card_type")
+    private String cardType;
+
+    @Column(name = "total_limit")
+    private int totalLimit;
+
+    @Column(name = "amount_used")
+    private int amountUsed;
+
+    @Column(name = "available_amount")
+    private int availableAmount;
+
+}
