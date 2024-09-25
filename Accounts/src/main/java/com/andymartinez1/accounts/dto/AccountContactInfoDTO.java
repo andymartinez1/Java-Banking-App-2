a8143b3a58bfec1a25970bfa1c9e288baf8bfbea
Andrew Martinez <andy72391@gmail.com>
2024-09-25 00:06:14 -0400

    Added an encryption key to hide email addresses in the Banking App 2 Config repository.

--- Accounts/src/main/java/com/andymartinez1/accounts/dto/AccountContactInfoDTO.java
+++ Accounts/src/main/java/com/andymartinez1/accounts/dto/AccountContactInfoDTO.java
@@ -1,12 +1,21 @@
 package com.andymartinez1.accounts.dto;
 
+import lombok.Getter;
+import lombok.Setter;
 import org.springframework.boot.context.properties.ConfigurationProperties;
 
 import java.util.List;
 import java.util.Map;
 
 @ConfigurationProperties(prefix = "accounts")
-public record AccountContactInfoDTO(String message,
-                                    Map<String, String> contactDetails,
-                                    List<String> onCallSupport) {
+@Getter
+@Setter
+public class AccountContactInfoDTO {
+
+    private String message;
+
+    private Map<String, String> contactDetails;
+
+    private List<String> onCallSupport;
+
 }
--- Cards/src/main/java/com/andymartinez1/cards/dto/CardContactInfoDTO.java
+++ Cards/src/main/java/com/andymartinez1/cards/dto/CardContactInfoDTO.java
@@ -1,12 +1,21 @@
 package com.andymartinez1.cards.dto;
 
+import lombok.Getter;
+import lombok.Setter;
 import org.springframework.boot.context.properties.ConfigurationProperties;
 
 import java.util.List;
 import java.util.Map;
 
 @ConfigurationProperties(prefix = "cards")
-public record CardContactInfoDTO(String message,
-                                 Map<String, String> contactDetails,
-                                 List<String> onCallSupport) {
+@Getter
+@Setter
+public class CardContactInfoDTO {
+
+    private String message;
+
+    private Map<String, String> contactDetails;
+
+    private List<String> onCallSupport;
+
 }
